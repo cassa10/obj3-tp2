5182d3784627a6f5e4d82cee8f5f02f2e0425539
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-11-16 09:27:13 -0300

    Construye Analizador con reglas en su constructor

--- src/main/scala/o3/lepifyo/tp2/analisis/Analizador.scala
+++ src/main/scala/o3/lepifyo/tp2/analisis/Analizador.scala
@@ -2,9 +2,9 @@ package o3.lepifyo.tp2.analisis
 
 import o3.lepifyo.tp2.ast.ElementoAST
 
-class Analizador {
+class Analizador(val reglas : List[Regla]) {
 
-  def analizar(ast: List[ElementoAST], reglas: List[Regla]): List[Problema] = ast.flatMap(
+  def analizar(ast: List[ElementoAST]): List[Problema] = ast.flatMap(
     _.analizarse(reglas).filter(_.isDefined).map(_.get)
   )
 
--- src/test/scala/o3/lepifyo/tp2/AnalizadorSpec.scala
+++ src/test/scala/o3/lepifyo/tp2/AnalizadorSpec.scala
@@ -9,19 +9,15 @@ import org.scalatest.matchers.should.Matchers
 
 class AnalizadorSpec extends AnyFunSpec with Matchers {
 
-  val parser = ParserFactory.build
-
-  // TODO: Construir el analizador ya con las reglas seteadas
-  val analizador = new Analizador
-
-  val reglas = List(new DivisionPorCero, new OperacionReduntante)
+  private val parser = ParserFactory.build
+  private val analizador = new Analizador(List(new DivisionPorCero, new OperacionReduntante))
 
   describe("análisis de programa") {
 
     it("No se encuentran problemas al analizar un programa correcto") {
       val ast = parser.parsear("1 + 1")
 
-      val problemasEncontrados = analizador.analizar(ast, reglas)
+      val problemasEncontrados = analizador.analizar(ast)
 
       problemasEncontrados shouldBe empty
     }
@@ -31,7 +27,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra un error al analizar un programa que contiene una división por cero") {
         val ast = parser.parsear("1 / 0")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         // TODO: Refactorizar los assert de las listas. Podemos validar que tiene un solo elemento y que es el elem que esperamos
         problemasEncontrados should have size 1
@@ -41,7 +37,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentran errores al analizar un programa que contiene mas de una división por cero") {
         val ast = parser.parsear("100 + 5 \n 100 + (1 / 0) / 20 \n (50 / 0)")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 2
         problemasEncontrados.head shouldBe Problema("No se puede dividir por cero", NivelGravedad.Error, DivisionAST(NumeroLiteral(1), NumeroLiteral(0)))
@@ -55,7 +51,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente se sume cero") {
         val ast = parser.parsear("1 + 0")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Sumar cero es redundate", NivelGravedad.Advertencia, SumaAST(NumeroLiteral(1), NumeroLiteral(0)))
@@ -64,7 +60,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente a cero le sea sumado otro número") {
         val ast = parser.parsear("0 + 1")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Sumar cero es redundate", NivelGravedad.Advertencia, SumaAST(NumeroLiteral(0), NumeroLiteral(1)))
@@ -73,7 +69,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente se reste cero") {
         val ast = parser.parsear("1 - 0")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Restar cero es redundate", NivelGravedad.Advertencia, RestaAST(NumeroLiteral(1), NumeroLiteral(0)))
@@ -82,7 +78,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente se multiplique por uno") {
         val ast = parser.parsear("2 * 1")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Multiplicar por uno es redundate", NivelGravedad.Advertencia, MultiplicacionAST(NumeroLiteral(2), NumeroLiteral(1)))
@@ -91,7 +87,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente a uno se lo multiplique por otro número") {
         val ast = parser.parsear("1 * 2")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Multiplicar por uno es redundate", NivelGravedad.Advertencia, MultiplicacionAST(NumeroLiteral(1), NumeroLiteral(2)))
@@ -100,7 +96,7 @@ class AnalizadorSpec extends AnyFunSpec with Matchers {
       it("Se encuentra una advertencia al analizar un programa en el que explícitamente se divida por uno") {
         val ast = parser.parsear("2 / 1")
 
-        val problemasEncontrados = analizador.analizar(ast, reglas)
+        val problemasEncontrados = analizador.analizar(ast)
 
         problemasEncontrados should have size 1
         problemasEncontrados.head shouldBe Problema("Dividir por uno es redundate", NivelGravedad.Advertencia, DivisionAST(NumeroLiteral(2), NumeroLiteral(1)))
