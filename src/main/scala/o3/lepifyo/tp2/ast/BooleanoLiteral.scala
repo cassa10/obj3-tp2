e12cfac391da4d8b60f515c352b85646ea35ad05
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-11-17 09:03:32 -0300

    Permite la aplicacion de reglas a valores literales

--- src/main/scala/o3/lepifyo/tp2/ast/BooleanoLiteral.scala
+++ src/main/scala/o3/lepifyo/tp2/ast/BooleanoLiteral.scala
@@ -5,8 +5,7 @@ case class BooleanoLiteral(valor: Boolean) extends ElementoAST {
 
   override def evaluarse(): BooleanoLiteral = this
 
-  //FIXME: Analizar si permitir que alguna regla ne permitan escribir algun booleano literal? Permitirselo a todos
-  override def analizarse(reglas: List[Regla]): List[Option[Problema]] = List(None)
+  override def analizarse(reglas: List[Regla]): List[Option[Problema]] = reglas.map(regla => regla.apply(this))
 
   override def toString: String = valor.toString
 
--- src/main/scala/o3/lepifyo/tp2/ast/NumeroLiteral.scala
+++ src/main/scala/o3/lepifyo/tp2/ast/NumeroLiteral.scala
@@ -6,8 +6,7 @@ case class NumeroLiteral(valor: Int) extends ElementoAST {
 
   override def evaluarse(): NumeroLiteral = this
 
-  //FIXME: Analizar si permitir que alguna regla ne permitan escribir algun numero literal?
-  def analizarse(reglas: List[Regla]): List[Option[Problema]] = List(None)
+  def analizarse(reglas: List[Regla]): List[Option[Problema]] = reglas.map(regla => regla.apply(this))
 
   override def toString: String = valor.toString
 
